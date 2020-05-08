3214a9738f03cab4ce039cd7ef1894a2145b2d88
NGUYEN NHAT LONG <NHATLONG@diginetcorp.com>
2020-05-08 17:08:20 +0700

    3

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const http=require('http');
 const port = process.env.PORT || 3000
 const app =require('./app');
 const server=http.createServer(app);
-
+const io=require('socket.io')(server);
 server.listen(port);
+
+io.on("connection",function(socket){
+    console.log('có người vừa kết nối')
+    socket.on('sendTest',function(data1){
+        console.log(data1)
+    })
+})
+
+
 console.log('RESTful API server started on: ' + port)
